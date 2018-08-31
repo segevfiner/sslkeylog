c771c184362f5c383e10996e772a104bd8b061e1
Segev Finer <segev208@gmail.com>
2018-08-31 13:10:37 +0300

    Document the sslkeylog module

--- sslkeylog.py
+++ sslkeylog.py
@@ -1,3 +1,16 @@
+"""
+This module provides a facility for logging SSL/TLS keys that can be used for decrypting
+SSL/TLS connections.
+
+Quickstart::
+
+    import sslkeylog
+
+    sslkeylog.set_keylog("sslkeylog.txt")
+
+    # Do anything involving SSL (Using the built-in ssl module)
+"""
+
 from __future__ import absolute_import
 import sys
 import atexit
@@ -15,9 +28,12 @@ else:
 
 
 def get_client_random(sock):
+    """Get the client random from an :class:`ssl.SSLSocket` or :class:`ssl.SSLObject`."""
     if sock is None:
-        raise TypeError("get_client_random() argument must be ssl.SSLSocket, not None")
+        raise TypeError(
+            "get_client_random() argument must be ssl.SSLSocket or ssl.SSLObject, not None")
 
+    # Some Python versions implement SSLSocket using SSLObject so we need to dereference twice
     sock = getattr(sock, '_sslobj', sock)
     sock = getattr(sock, '_sslobj', sock)
     if sock is None:
@@ -27,9 +43,12 @@ def get_client_random(sock):
 
 
 def get_master_key(sock):
+    """Get the master key from an :class:`ssl.SSLSocket` or :class:`ssl.SSLObject`."""
     if sock is None:
-        raise TypeError("get_master_key() argument must be ssl.SSLSocket, not None")
+        raise TypeError(
+            "get_master_key() argument must be ssl.SSLSocket or ssl.SSLObject, not None")
 
+    # Some Python versions implement SSLSocket using SSLObject so we need to dereference twice
     sock = getattr(sock, '_sslobj', sock)
     sock = getattr(sock, '_sslobj', sock)
     if sock is None:
@@ -39,6 +58,7 @@ def get_master_key(sock):
 
 
 def get_keylog_line(sock):
+    """Generate a key log line from an :class:`ssl.SSLSocket` or :class:`ssl.SSLObject`."""
     return "CLIENT_RANDOM {} {}".format(
         binascii.hexlify(get_client_random(sock)).decode("utf-8"),
         binascii.hexlify(get_master_key(sock)).decode("utf-8"))
@@ -56,6 +76,17 @@ def _cleanup():
 
 
 def set_keylog(dest):
+    """
+    Set the key log to *dest* which can be either a path, a file-like object or a callback.
+
+    The key log is process-wide and will log keys for all SSL/TLS connections in the process.
+
+    A callback will be called with the socket, and a key log line which should be written
+    to the key log.
+
+    This will apply the monkey patch needed to implement this if it's not already applied,
+    see :func:`.patch`.
+    """
     global _keylog_callback, _log_file
 
     patch()
@@ -110,6 +141,7 @@ if hasattr(ssl, 'SSLObject'):
 
 
 def patch():
+    """Apply the monkey patch used to implement the key log, if not already patched."""
     global _patched, _orig_sslsocket_do_handshake, _orig_sslobject_do_handshake
 
     if _patched:
@@ -126,6 +158,7 @@ def patch():
 
 
 def unpatch():
+    """Unapply the monkey patch used to implement the key log, if it was applied."""
     global _patched, _orig_sslsocket_do_handshake, _orig_sslobject_do_handshake
 
     if not _patched:
