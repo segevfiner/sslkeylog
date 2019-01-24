b36d57b7466d543c9e321d951ffc7cbb839d63d6
Segev Finer <segev208@gmail.com>
2019-01-24 18:18:44 +0200

    Add GitHub button to docs

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,12 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_button': True,
+    'github_user': 'segevfiner',
+    'github_repo': 'sslkeylog',
+    'github_type': 'star',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
