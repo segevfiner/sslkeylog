0b677bdb6be85e96a3f3903a96f19e13b5935154
Segev Finer <segev208@gmail.com>
2019-10-31 17:46:37 +0200

    Add Python 3.8 classifier

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords="ssl tls sslkeylogfile",
     zip_safe=False,
