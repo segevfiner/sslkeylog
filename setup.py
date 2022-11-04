0c37e23d3d1c9e36d4e656879ea3a5e5e7b0dfa3
Segev Finer <segev208@gmail.com>
2022-11-04 09:23:16 +0200

    Skip flake8 on Python 3.7 :(

--- setup.py
+++ setup.py
@@ -69,7 +69,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
     ],
     keywords="ssl tls sslkeylogfile",
     zip_safe=False,
